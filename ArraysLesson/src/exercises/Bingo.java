efe657e89883e80f01c8ffd18aea39d95113a2b1
SimoNeru <102528993+SimNeru@users.noreply.github.com>
2024-04-10 12:53:38 +0200

    Bingo update exercise

--- ArraysLesson/src/exercises/Bingo.java
+++ ArraysLesson/src/exercises/Bingo.java
@@ -16,7 +16,6 @@ public class Bingo {
 		System.out.println("Hello player choose how many numbers you would like to extract");
 		numbersToExtract = input.nextInt();
 
-
 		// controllo che l'utente inserisca un numero in un range accettabile
 		while (numbersToExtract < 1 || numbersToExtract > 90) {
 			System.out.println("You are out of range, please digit a set of numbers to extract beetween 1 and 90");
@@ -27,10 +26,42 @@ public class Bingo {
 
 		// genero la lista e il value che conterrà il numero generato randomicamente
 		List<Integer> numbers = new ArrayList<>();
-		int numberGenerated = 0;
-
-		// popolo la lista di numberi finché non differiscono da numeri già presenti
-		/*while(counter<numbersToExtract) 
+		
+		// SISTEMA 2
+		// Popolo l'array di numbers con dei numeri da 1 a 90
+		for (int i=0;i<90;) 
+		{
+			numbers.add(i, ++i);
+		}
+		
+		// genero una seconda lista su cui assegnerò i valori dell'indice estratti
+		List<Integer> extractedNumbers = new ArrayList<>();
+		int randomPosition = 0;
+		int positionsToRemove = 0;
+		
+		// estraggo randomicamente l'indice della lista di numeri di partenza, assieme all'elemento presente in quella posizione
+		// aggiungo l'elemento presente alla seconda lista di numeri estratti e lo rimuovo dalla lista precedente
+		for (int i=0;i<numbersToExtract;i++) 
+		{
+			randomPosition = (int)Math.ceil(Math.random()*(numbers.size()));
+			
+			int value = numbers.get(randomPosition);
+			// assegna alla seconda array il numero estratto
+			extractedNumbers.add(i, value);
+			// rimuove dall'originale il numero estratto
+			numbers.remove(randomPosition);
+		}
+		
+		System.out.println("Extrated values:");
+		for (Integer value : extractedNumbers) {
+			System.out.print(value + " ");
+		}
+		
+		/*
+		// SISTEMA 1: popolo la lista di numberi finché non differiscono da numeri già presenti
+		
+		// USANDO UN CICLO WHILE  
+		while(counter<numbersToExtract) 
 		{
 			numberGenerated = r.nextInt(1, 91);
 			
@@ -43,8 +74,9 @@ public class Bingo {
 			{
 				continue;
 			}
-		}*/
+		}
 		
+		// USANDO UN CICLO FOR
 		for(int i=0; i<numbersToExtract;) 
 		{
 			numberGenerated = r.nextInt(1, 91);
@@ -58,7 +90,7 @@ public class Bingo {
 		// stampo la lista dei numeri generati
 		for (Integer number : numbers) {
 			System.out.print(number + " ");
-		}
+		}*/
 	}
 
 }
