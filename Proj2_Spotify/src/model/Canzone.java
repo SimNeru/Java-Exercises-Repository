22afd4de4e6325de00edfa9edccfd3a60f564cf4
Simone Nerucci <102528993+SimNeru@users.noreply.github.com>
2024-04-27 12:01:04 +0200

    Exercise Spotify completion

--- Proj2_Spotify/src/model/Canzone.java
+++ Proj2_Spotify/src/model/Canzone.java
@@ -2,8 +2,8 @@ package model;
 
 public class Canzone {
 
-	public String titolo;
-	public String cantante;
+	String titolo;
+	String cantante;
 	
 	public Canzone(String titolo, String cantante) {
 		this.titolo = titolo;
@@ -18,5 +18,15 @@ public class Canzone {
 	@Override
 	public String toString() {
 		return "Canzone [titolo=" + titolo + ", cantante=" + cantante + "]";
+	}
+	
+	public String getTitolo() 
+	{
+		return this.titolo;
+	}
+	
+	public String getCantante() 
+	{
+		return this.cantante;
 	}
 }
